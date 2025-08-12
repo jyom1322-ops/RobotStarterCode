ae78980ff9dafd4bcc3b00df52ca544ff12c8580
jyom1322-ops <jm1278@pleasantonusd.net>
2025-08-12 22:21:45 +0000

    First Day Initials

--- Main.java
+++ Main.java
@@ -7,21 +7,84 @@ public class Main implements Directions {
   public static void main(String[] args) 
 	{
 		//My robot's name is karel
-    Robot karel = new Robot(5,1,East,9);
+    Robot karel = new Robot(9,4,East,100);
     World.setVisible(true);
     World.setSize(10,10);
     World.setDelay(100);
 
+    Robot andy = new Robot(9,6,East,100);
+    World.setSize(10,10);
+    World.setDelay(100);
+
+    turnRight(karel);
+    karel.putBeeper();
 		karel.move();
     karel.putBeeper();
     karel.move();
-    karel.move();
-    karel.turnLeft();
-
+    karel.putBeeper();
     karel.move();
     karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    turnRight(karel);
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    turnRight(karel);
+    karel.move();
     karel.putBeeper();
 
+    andy.putBeeper();
+    andy.move();
+    turnRight(andy);
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.turnLeft();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.turnLeft();
+    andy.move();
+    andy.putBeeper();
+    turnRight(andy);
+    andy.move();
+    andy.turnLeft();
+    andy.move();
+    andy.putBeeper();
+    andy.turnLeft();
+    andy.turnLeft();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+    turnRight(andy);
+    andy.move();
+    andy.move();
+    andy.move();
+    andy.move();
+    andy.putBeeper();
+    turnRight(andy);
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+    andy.move();
+    andy.putBeeper();
+
+    }
+    public static void turnRight(Robot placeholder){
+      placeholder.turnLeft();
+      placeholder.turnLeft();
+      placeholder.turnLeft();
   }
-    
 }
